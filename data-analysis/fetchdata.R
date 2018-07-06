79cd5e0e729513e6fd903650c629905468c7014c
Alan Franzoni <username@franzoni.eu>
2018-07-06 16:52:39 +0200

    Enh: extract data from live worksheet

--- data-analysis/fetchdata.R
+++ data-analysis/fetchdata.R
@@ -1,1 +1,16 @@
-install.packages("googledrive")
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(googlesheets, dplyr)
+
+library(googlesheets)
+suppressMessages(library(dplyr))
+
+misaligned_answers_sheet <- gs_key("1KwhoGLuazPAeUcsVb_a3LwPCjXHQmuoTSn-KJwOXOXI")
+
+gs_ws_ls(misaligned_answers_sheet)
+df <- gs_read(misaligned_answers_sheet, ws = "Risposte del modulo 1")
+
+
+
+
+  
+
