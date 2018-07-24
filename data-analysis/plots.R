8c8806285cc28051adaf7dbd11526d367135a86d
Alan Franzoni <username@franzoni.eu>
2018-07-24 22:57:07 +0200

    Enh: basic comparator graph

--- data-analysis/plots.R
+++ data-analysis/plots.R
@@ -9,3 +9,11 @@ age.group.chart <- ggplot(age.group.bsc.achieves.table, aes(x="", y=total, fill=
 #degree.highest.chart <- ggplot(degree.highest.bsc.achieves.table, aes(x="", y=total, fill=degree.highest)) + geom_bar(stat="identity")
 #degree.country.chart <- ggplot(degree.country.bsc.achieves.table, aes(x="", y=total, fill=degree.country)) + geom_bar(stat="identity")
 #employed.country.chart <- ggplot(employed.country.bsc.achieves.table, aes(x="", y=total, fill=employed.country)) + geom_bar(stat="identity")
+
+asd <- gather(tibble::rownames_to_column(categories.bsc.achieves.table), achievement, percentage, bsc.achieves.programming_pc:bsc.achieves.dontknow_pc, factor_key=TRUE)
+
+# we need to find a better way to put the names on the bottom, but the graph should be ok
+chart <- ggplot(asd, aes(x=achievement, y=percentage, fill=rowname)) +  geom_bar(position="dodge", stat="identity")
+
+
+
