28518768a870996e74e209ba52d667414c1bab5f
Alan Franzoni <username@franzoni.eu>
2018-07-25 12:23:49 +0200

    Enh: better naming

--- data-analysis/plots.R
+++ data-analysis/plots.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(dplyr)
 #source("answers_pare.R")
 
 
@@ -10,10 +11,13 @@ age.group.chart <- ggplot(age.group.bsc.achieves.table, aes(x="", y=total, fill=
 #degree.country.chart <- ggplot(degree.country.bsc.achieves.table, aes(x="", y=total, fill=degree.country)) + geom_bar(stat="identity")
 #employed.country.chart <- ggplot(employed.country.bsc.achieves.table, aes(x="", y=total, fill=employed.country)) + geom_bar(stat="identity")
 
-asd <- gather(tibble::rownames_to_column(categories.bsc.achieves.table), achievement, percentage, bsc.achieves.programming_pc:bsc.achieves.dontknow_pc, factor_key=TRUE)
+asd <- gather(tibble::rownames_to_column(categories.bsc.achieves.table %>% select(contains("_pc")), var="category"), achievement, percentage, bsc.achieves.programming_pc:bsc.achieves.dontknow_pc, factor_key=TRUE)
+asd <- asd %>% mutate(achievement = gsub("_pc", "", gsub("bsc.achieves.", "", achievement, fixed=TRUE), fixed=TRUE),
+                      category = gsub(".", " ", category, fixed = TRUE))
 
-# we need to find a better way to put the names on the bottom, but the graph should be ok
-chart <- ggplot(asd, aes(x=achievement, y=percentage, fill=rowname)) +  geom_bar(position="dodge", stat="identity")
+
+chart <- ggplot(asd, aes(x=achievement, y=percentage, fill=category)) +  geom_bar(position="dodge", stat="identity")
+ggsave("/tmp/something.eps", plot=chart, device="eps", width=50, height=10, units="cm", dpi=72)
 
 
 
