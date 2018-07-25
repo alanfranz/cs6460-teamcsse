e8b6c16310b5a3142e79ff8513cf06fe18cf3c4a
Alan Franzoni <username@franzoni.eu>
2018-07-25 13:23:53 +0200

    More plotting

--- data-analysis/plots.R
+++ data-analysis/plots.R
@@ -11,13 +11,20 @@ age.group.chart <- ggplot(age.group.bsc.achieves.table, aes(x="", y=total, fill=
 #degree.country.chart <- ggplot(degree.country.bsc.achieves.table, aes(x="", y=total, fill=degree.country)) + geom_bar(stat="identity")
 #employed.country.chart <- ggplot(employed.country.bsc.achieves.table, aes(x="", y=total, fill=employed.country)) + geom_bar(stat="identity")
 
-asd <- gather(tibble::rownames_to_column(categories.bsc.achieves.table %>% select(contains("_pc")), var="category"), achievement, percentage, bsc.achieves.programming_pc:bsc.achieves.dontknow_pc, factor_key=TRUE)
-asd <- asd %>% mutate(achievement = gsub("_pc", "", gsub("bsc.achieves.", "", achievement, fixed=TRUE), fixed=TRUE),
-                      category = gsub(".", " ", category, fixed = TRUE))
+categories_plot <- function(mytable, title, replacesth) {
+
+ asd <- gather(tibble::rownames_to_column(mytable %>% select(contains("_pc")), var="category"), achievement, percentage, matches(".*_pc$"), factor_key=TRUE)
+ asd <- asd %>% mutate(achievement = gsub("_pc", "", gsub(replacesth, "", achievement, fixed=TRUE), fixed=TRUE),
+                      category = gsub("$", "?", gsub(".", " ", category, fixed = TRUE)))
 
 
-chart <- ggplot(asd, aes(x=achievement, y=percentage, fill=category)) +  geom_bar(position="dodge", stat="identity")
-ggsave("/tmp/something.eps", plot=chart, device="eps", width=50, height=10, units="cm", dpi=72)
+ chart <- ggplot(asd, aes(x=achievement, y=percentage, fill=category)) +  geom_bar(position="dodge", stat="identity") + labs(title=title, y="percentage of category respondents")
+ ggsave(sprintf("%s/%s.eps", "plots_output", gsub(" ", "_", title, fixed=TRUE)), plot=chart, device="eps", width=50, height=10, units="cm", dpi=72)
+}
 
+categories_plot(categories.bsc.achieves.table, "BSc graduate reported achievements by category", "bsc.achieves.")
+categories_plot(categories.bsc.shouldachieve.table, "BSc graduate would-like achievements by category", "bsc.shouldachieve.")
+categories_plot(categories.msc.achieves.table, "MSc graduate reported achievements by category", "msc.achieves.")
+categories_plot(categories.msc.shouldachieve.table, "MSc graduate would-like achievements by category", "msc.shouldachieve.")
 
 
