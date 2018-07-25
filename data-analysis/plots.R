a7ac80d8de5cb136b0179f96b51163fab3d83e19
Alan Franzoni <username@franzoni.eu>
2018-07-25 17:01:06 +0200

    Enh: experience/job comparison

--- data-analysis/plots.R
+++ data-analysis/plots.R
@@ -39,3 +39,14 @@ categories_plot(categories.msc.shouldachieve.table, "MSc graduate would-like ach
 categories_plot(categories.msc.hireability.gpa.table, "MSc graduate reported GPA impact on hireability by category", "IGNOREIGNORE")
 categories_plot(categories.msc.landjob.delay.table, "MSc graduate reported delay to get first job by category", "IGNOREIGNORE")
 categories_plot(categories.msc.proficiency.delay.table, "MSc graduate reported delay to achieve proficiency", "IGNOREIGNORE")
+
+categories_plot(categories.retraining.table, "Reported retraining best options", "retraining.alt.")
+
+# may need better formatting/data cleaning
+categories_plot(categories.bsc.vs.jobexperience.hireability.table, "BSc degree versus job experience hireability", "IGNORE")
+categories_plot(categories.bsc.vs.jobexperience.shorttermproficiency.table, "BSc degree versus job experience short-term proficiency", "IGNORE")
+categories_plot(categories.bsc.vs.jobexperience.longtermproficiency.table, "BSc degree versus job experience long-term proficiency", "IGNORE")
+categories_plot(categories.msc.vs.bscexperience.hireability.table, "MSc degree versus BSC and job experience hireability", "IGNORE")
+categories_plot(categories.msc.vs.bscexperience.shorttermproficiency.table, "MSc degree versus BSC and job experience short-term proficiency", "IGNORE")
+categories_plot(categories.msc.vs.bscexperience.longtermproficiency.table, "MSc degree versus BSC and job experience long-term proficiency", "IGNORE")
+
